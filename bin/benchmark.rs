11314d53d690e26cd12d55696b9078e04c61ffdb
Emad Heydari Beni <emad@heydari.be>
2024-12-26 10:58:22 +0100

    Update benchmarks

--- bin/benchmark.rs
+++ bin/benchmark.rs
@@ -5,6 +5,6 @@ fn main() {
     println!("Benchmarking...");
 
     benchmark::benchmark_spmac_spseq::start_benchmarking();
-    // benchmark::benchmark_pairing_less::start_benchmarking();
-    // benchmark::benchmark_paring_based::start_benchmarking()
+    benchmark::benchmark_pairing_less::start_benchmarking();
+    benchmark::benchmark_paring_based::start_benchmarking()
 }
